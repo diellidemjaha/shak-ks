1bd976915f7ae851e5cb00776c19004646d7718f
Dielli Demjaha <diellidemjaha@gmail.com>
2024-04-02 12:19:51 +0200

    Update header.php

--- shak-ks_theme/header.php
+++ shak-ks_theme/header.php
@@ -6,8 +6,6 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"> -->
-    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script> -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <?php wp_head(); ?>
@@ -65,10 +63,8 @@
 
             <?php
             if (is_user_logged_in()) {
-                // If user is logged in, show logout button
                 echo '<a class="btn btn-outline-light rounded-5" href="' . wp_logout_url(home_url()) . '">Log out</a>';
             } else {
-                // If user is not logged in, show login button
                 echo '<a class="btn btn-outline-light rounded-5" href="' . get_stylesheet_directory_uri() . '/login-form/">Log in</a>';
             }
             ?>
